2f83af05220b4bb880efdb0fcd86f809b8490d1e
MrMelon54 <github@mrmelon54.com>
2023-06-23 23:00:09 +0100

    Add http acme provider

--- renewal/http-acme-provider.go
+++ renewal/http-acme-provider.go
@@ -1,7 +1,10 @@
 package renewal
 
 import (
+	"fmt"
 	"github.com/go-acme/lego/v4/challenge"
+	"net/http"
+	"strings"
 )
 
 var _ challenge.Provider = &HttpAcmeProvider{}
@@ -9,18 +12,47 @@ var _ challenge.Provider = &HttpAcmeProvider{}
 type HttpAcmeProvider struct {
 	accessToken, refreshToken    string
 	apiUrlPresent, apiUrlCleanUp string
+	trip                         http.RoundTripper
 }
 
 func NewCustomHTTPProvider(accessToken, refreshToken, apiUrlPresent, apiUrlCleanUp string) *HttpAcmeProvider {
-	return &HttpAcmeProvider{accessToken, refreshToken, apiUrlPresent, apiUrlCleanUp}
+	return &HttpAcmeProvider{accessToken, refreshToken, apiUrlPresent, apiUrlCleanUp, http.DefaultTransport}
 }
 
 func (h *HttpAcmeProvider) Present(domain, token, keyAuth string) error {
-	//TODO implement me
-	panic("implement me")
+	v := strings.NewReplacer("%domain%", domain, "%token%", token, "%content%", keyAuth).Replace(h.apiUrlPresent)
+	req, err := http.NewRequest(http.MethodPut, v, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+h.accessToken)
+
+	// round trip
+	trip, err := h.trip.RoundTrip(req)
+	if err != nil {
+		return err
+	}
+	if trip.StatusCode != http.StatusOK {
+		return fmt.Errorf("Trip response status code was not 200")
+	}
+	return nil
 }
 
 func (h *HttpAcmeProvider) CleanUp(domain, token, keyAuth string) error {
-	//TODO implement me
-	panic("implement me")
+	v := strings.NewReplacer("%domain%", domain, "%token%", token, "%content%", keyAuth).Replace(h.apiUrlCleanUp)
+	req, err := http.NewRequest(http.MethodPut, v, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+h.accessToken)
+
+	// round trip
+	trip, err := h.trip.RoundTrip(req)
+	if err != nil {
+		return err
+	}
+	if trip.StatusCode != http.StatusOK {
+		return fmt.Errorf("Trip response status code was not 200")
+	}
+	return nil
 }
--- renewal/http-acme-provider_test.go
+++ renewal/http-acme-provider_test.go
@@ -1,1 +1,104 @@
 package renewal
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+	"github.com/MrMelon54/mjwt"
+	"github.com/MrMelon54/mjwt/auth"
+	"github.com/MrMelon54/mjwt/claims"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+	"time"
+)
+
+type fakeTransport struct {
+	verify mjwt.Verifier
+	req    *http.Request
+	clean  bool
+}
+
+func (f *fakeTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	bearer := req.Header.Get("Authorization")
+	if !strings.HasPrefix(bearer, "Bearer ") {
+		return nil, fmt.Errorf("invalid bearer token")
+	}
+	_, b, err := mjwt.ExtractClaims[auth.AccessTokenClaims](f.verify, bearer[7:])
+	if err != nil {
+		return nil, err
+	}
+
+	// check perms
+	if !f.clean && !b.Claims.Perms.Has("test:acme:present") {
+		return nil, fmt.Errorf("missing perm 'test:acme:present'")
+	}
+	if f.clean && !b.Claims.Perms.Has("test:acme:clean") {
+		return nil, fmt.Errorf("missing perm 'test:acme:clean'")
+	}
+	rec := httptest.NewRecorder()
+	rec.WriteHeader(http.StatusOK)
+	f.req = req
+	return rec.Result(), nil
+}
+
+func TestHttpAcmeProvider_Present(t *testing.T) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	assert.NoError(t, err)
+
+	// perms
+	ps := claims.NewPermStorage()
+	ps.Set("test:acme:present")
+
+	// signer
+	signer := mjwt.NewMJwtSigner("Test", privateKey)
+	accessToken, err := signer.GenerateJwt("", "", nil, 5*time.Minute, auth.AccessTokenClaims{Perms: ps})
+	assert.NoError(t, err)
+
+	ft := &fakeTransport{verify: signer}
+	prov := &HttpAcmeProvider{
+		accessToken,
+		"",
+		"https://api.example.com/acme/present/%domain%/%token%/%content%",
+		"https://api.example.com/acme/clean/%domain%/%token%",
+		ft,
+	}
+	assert.NoError(t, prov.Present("example.com", "1234", "1234abcd"))
+	assert.Equal(t, *ft.req.URL, url.URL{
+		Scheme: "https",
+		Host:   "api.example.com",
+		Path:   "/acme/present/example.com/1234/1234abcd",
+	})
+}
+
+func TestHttpAcmeProvider_CleanUp(t *testing.T) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	assert.NoError(t, err)
+
+	// perms
+	ps := claims.NewPermStorage()
+	ps.Set("test:acme:clean")
+
+	// signer
+	signer := mjwt.NewMJwtSigner("Test", privateKey)
+	accessToken, err := signer.GenerateJwt("", "", nil, 5*time.Minute, auth.AccessTokenClaims{Perms: ps})
+	assert.NoError(t, err)
+
+	ft := &fakeTransport{verify: signer, clean: true}
+	prov := &HttpAcmeProvider{
+		accessToken,
+		"",
+		"https://api.example.com/acme/present/%domain%/%token%/%content%",
+		"https://api.example.com/acme/clean/%domain%/%token%",
+		ft,
+	}
+	assert.NoError(t, prov.CleanUp("example.com", "1234", "1234abcd"))
+	assert.Equal(t, *ft.req.URL, url.URL{
+		Scheme: "https",
+		Host:   "api.example.com",
+		Path:   "/acme/clean/example.com/1234",
+	})
+}
